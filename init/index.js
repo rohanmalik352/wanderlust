82ee3a218f54249611d78647528a676d4f9e63ac
Rohan Malik <rohanmalik801@gmail.com>
2025-12-25 19:29:42 +0530

    Make filters responsive on small screens

--- init/index.js
+++ init/index.js
@@ -1,24 +1,33 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: "../.env" });
 const data = require("./data.js");
 const Listing = require("../models/listing.js");
 
+const dbUrl = process.env.ATLASDB_URL;
+
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
-    console.log("connection successful");
-}
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("MongoDB Atlas connected");
 
-main().catch(err => console.log(err));
-
-const initDb = async () => {
     await Listing.deleteMany({});
 
     const listings = data.data.map(obj => ({
-        ...obj,
-        owner: new mongoose.Types.ObjectId("693e551236ef44f23d39256c")
+      ...obj,
+      geometry: {
+        type: "Point",
+        coordinates: [77.2090, 28.6139] // Delhi (lon, lat)
+      },
+      owner: new mongoose.Types.ObjectId("693e551236ef44f23d39256c")
     }));
 
     await Listing.insertMany(listings);
-    console.log("data was initialized");
-};
+    console.log("Data uploaded successfully");
 
-initDb();
+    await mongoose.connection.close();
+  } catch (err) {
+    console.error("ERROR:", err.message);
+  }
+}
+
+main();
